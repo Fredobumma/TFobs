42535f4bd70b1cd783675d9be4ddcbf09b418ed3
Manfred Obumma <fredobumma@gmail.com>
2024-07-05 12:36:36 +0100

    --feat:: add loading

--- sections/pricing.jsx
+++ sections/pricing.jsx
@@ -35,7 +35,7 @@ const Pricing = () => {
 						<Link
 							href=""
 							aria-describedby="tier-standard"
-							className="ring-1 ring-inset ring-white hover:ring-white mt-6 block rounded-md py-2 px-3 text-center text-base font-medium leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#b69e57] hover:outline-[#b69e57] hover:outline-offset-2 hover:outline-2 hover:outline"
+							className="ring-1 ring-inset ring-white hover:ring-white mt-6 block rounded-md py-2 px-3 text-center text-base font-medium leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#b69e57] hover:outline-[#b69e57] hover:outline-offset-2 hover:outline-2 hover:outline transition ease-in-out duration-300"
 							target="_blank"
 						>
 							Subscribe Now
@@ -122,7 +122,7 @@ const Pricing = () => {
 						<Link
 							href=""
 							aria-describedby="tier-standard"
-							className="bg-white text-[#815316] mt-6 block rounded-md py-2 px-3 text-center text-base font-medium leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#b69e57] hover:outline-[#b69e57] hover:outline-offset-2 hover:outline-2 hover:outline"
+							className="bg-white text-[#815316] mt-6 block rounded-md py-2 px-3 text-center text-base font-medium leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#b69e57] hover:outline-[#b69e57] hover:outline-offset-2 hover:outline-2 hover:outline transition ease-in-out duration-300"
 							target="_blank"
 						>
 							Subscribe Now
--- sections/product.jsx
+++ sections/product.jsx
@@ -2,105 +2,128 @@
 
 import { useState } from "react";
 
+import { useToast } from "@/components/ui/use-toast";
 import { searchYt } from "@/app/searchyt";
 import Link from "next/link";
+import Loading from "@/app/loading";
 
 const Product = () => {
 	const [videos, setVideos] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	const { toast } = useToast();
 
 	const handleSubmit = async (e) => {
+		setLoading(true);
 		e.preventDefault();
 
-		const videos = await searchYt(e.target[0].value);
-		setVideos(videos);
+		try {
+			const videos = await searchYt(e.target[0].value);
+			setVideos(videos);
+		} catch (error) {
+			toast({ title: "Unable to get youtube content." });
+		} finally {
+			setLoading(false);
+		}
 	};
 
 	return (
-		<section id="product" className="pt-2.5">
-			<form onSubmit={handleSubmit}>
-				<label
-					className="mx-auto mt-10 relative bg-white min-w-sm max-w-2xl flex flex-col md:flex-row items-center justify-center border py-2 px-2 rounded-2xl gap-2 shadow-2xl focus-within:border-gray-300"
-					htmlFor="search-bar"
+		<>
+			{loading && <Loading />}
+			<section id="product" className="pt-2.5">
+				<form onSubmit={handleSubmit}>
+					<label
+						className="mx-auto mt-10 relative bg-white min-w-sm max-w-2xl flex flex-col md:flex-row items-center justify-center border py-2 px-2 rounded-2xl gap-2 shadow-2xl focus-within:border-gray-300"
+						htmlFor="search-bar"
+					>
+						<input
+							name="search"
+							type="search"
+							id="search-bar"
+							placeholder="Enter your keyword here"
+							className="px-6 py-2 w-full rounded-md flex-1 outline-none bg-white text-[#3d3d3d]"
+							minLength={3}
+							required
+							maxLength={50}
+						/>
+						<button className="w-full md:w-auto px-6 py-3 bg-black border-black text-white fill-white hover:scale-95 active:scale-95 border will-change-transform overflow-hidden relative rounded-lg transition ease-in-out duration-300 disabled:opacity-70">
+							<div className="relative">
+								<div className="flex items-center transition-all opacity-1 valid:">
+									<span className="text-sm font-semibold whitespace-nowrap truncate mx-auto">
+										Search
+									</span>
+								</div>
+							</div>
+						</button>
+					</label>
+				</form>
+				<div
+					className={`flex gap-5 justify-center flex-wrap transition ease-in-out duration-300 ${
+						videos.length ? "mt-20" : ""
+					}`}
 				>
-					<input
-						name="search"
-						type="search"
-						id="search-bar"
-						placeholder="your keyword here"
-						className="px-6 py-2 w-full rounded-md flex-1 outline-none bg-white text-[#3d3d3d]"
-						minLength={3}
-						required
-						maxLength={50}
-					/>
-					<button className="w-full md:w-auto px-6 py-3 bg-black border-black text-white fill-white active:scale-95 duration-100 border will-change-transform overflow-hidden relative rounded-lg transition-all disabled:opacity-70">
-						<div className="relative">
-							<div className="flex items-center transition-all opacity-1 valid:">
-								<span className="text-sm font-semibold whitespace-nowrap truncate mx-auto">
-									Search
-								</span>
-							</div>
-						</div>
-					</button>
-				</label>
-			</form>
-			<div
-				className={`flex gap-5 justify-center flex-wrap transition ease-in-out duration-300 ${
-					videos.length ? "mt-20" : ""
-				}`}
-			>
-				{videos.map(
-					(
-						{ thumbnail, channel, link, title, duration_raw, uploaded, views },
-						i,
-					) => (
-						<div
-							key={i}
-							className="p-4 max-h-fit min-w-[250px] max-w-sm border border-blue-600/30 rounded-2xl hover:shadow-xl flex flex-col items-center"
-							href="#"
-						>
-							<img
-								src={thumbnail}
-								className="shadow mb-5 rounded-lg overflow-hidden border"
-							/>
-							<div className="mt-auto">
-								<h4 className="font-bold text-xl">Channel: {channel.name}</h4>
-								<p className="mt-2.5 mb-[30px]">{title}</p>
-								<div class="flex justify-center">
-									<p>
-										Duration:
-										<span class="inline-block bg-black rounded-full px-3 py-1 text-sm font-semibold text-gray-300 opacity-80 mr-2 mb-2">
-											{duration_raw}
-										</span>
-									</p>
-									<p>
-										Uploaded:
-										<span class="inline-block bg-black rounded-full px-3 py-1 text-sm font-semibold text-gray-300 opacity-80 mr-2 mb-2">
-											{uploaded}
-										</span>
-									</p>
-									<p>
-										views:
-										<span class="inline-block bg-black rounded-full px-3 py-1 text-sm font-semibold text-gray-300 opacity-80 mr-2 mb-2">
-											{views.toLocaleString("en-US")}
-										</span>
-									</p>
-								</div>
+					{videos.map(
+						(
+							{
+								thumbnail,
+								channel,
+								link,
+								title,
+								duration_raw,
+								uploaded,
+								views,
+							},
+							i,
+						) => (
+							<div
+								key={i}
+								className="p-4 max-h-fit min-w-[250px] max-w-sm border border-blue-600/30 rounded-2xl hover:shadow-xl flex flex-col items-center"
+								href="#"
+							>
+								<img
+									src={thumbnail}
+									className="shadow mb-5 rounded-lg overflow-hidden border"
+								/>
+								<div className="mt-auto">
+									<h4 className="font-bold text-xl">Channel: {channel.name}</h4>
+									<p className="mt-2.5 mb-[30px]">{title}</p>
+									<div class="flex justify-center">
+										<p>
+											Duration:
+											<span class="inline-block bg-black rounded-full px-3 py-1 text-sm font-semibold text-gray-300 opacity-80 mr-2 mb-2">
+												{duration_raw}
+											</span>
+										</p>
+										<p>
+											Uploaded:
+											<span class="inline-block bg-black rounded-full px-3 py-1 text-sm font-semibold text-gray-300 opacity-80 mr-2 mb-2">
+												{uploaded}
+											</span>
+										</p>
+										<p>
+											views:
+											<span class="inline-block bg-black rounded-full px-3 py-1 text-sm font-semibold text-gray-300 opacity-80 mr-2 mb-2">
+												{views.toLocaleString("en-US")}
+											</span>
+										</p>
+									</div>
 
-								<div className="my-5">
-									<Link
-										href={link}
-										target="_blank"
-										className="block text-center rounded-lg border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-[#3d3d3d] shadow-sm hover:scale-95 focus:scale-95 transition ease-in-out duration-300"
-									>
-										open
-									</Link>
+									<div className="my-5">
+										<Link
+											href={link}
+											target="_blank"
+											className="block text-center rounded-lg border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-[#3d3d3d] shadow-sm hover:scale-95 focus:scale-95 transition ease-in-out duration-300"
+										>
+											open
+										</Link>
+									</div>
 								</div>
 							</div>
-						</div>
-					),
-				)}
-			</div>
-		</section>
+						),
+					)}
+				</div>
+			</section>
+		</>
 	);
 };
 
